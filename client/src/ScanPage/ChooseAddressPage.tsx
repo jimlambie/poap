99491d58ccdc94b236808ba7eb9bfd6d6d804a8c
James Lambie <jameslambie@gmail.com>
2021-03-10 07:37:50 +0000

    Update ChooseAddressPage.tsx
    This PR fixes a typo in the content, changing "off" to "of"

--- client/src/ScanPage/ChooseAddressPage.tsx
+++ client/src/ScanPage/ChooseAddressPage.tsx
@@ -122,7 +122,7 @@ export const ChooseAddressPage: React.FC<ChooseAddressPageProps> = ({ onAccountD
       <div className="container">
         <div className="content-event" data-aos="fade-up" data-aos-delay="300">
           <p>
-            The <span>Proof of attendance protocol</span> (POAP) reminds you off the <span>cool places</span> you’ve
+            The <span>Proof of attendance protocol</span> (POAP) reminds you of the <span>cool places</span> you’ve
             been to.
           </p>
           <br />
